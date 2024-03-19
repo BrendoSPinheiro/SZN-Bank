2509db641e1a8eb3092f7680d5821062b9d08b5b
BrendoSPinheiro <brendo.spinheiro@gmail.com>
2024-03-18 21:32:03 -0300

    chore: create the basic structure of the project

---
+++ src/main/java/br/com/brendosp/sznbank/SznBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.brendosp.sznbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SznBankApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SznBankApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/brendosp/sznbank/SznBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brendosp.sznbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SznBankApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
